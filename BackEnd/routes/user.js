0a566c64b60a82c56ff84e1fe66f0f0f4c53d8bf
Sarocha Sothornprapakorn <tua.sarocha@gmail.com>
2013-11-24 13:56:36 +0700

    signup api /api/users/signup action complete

--- BackEnd/routes/user.js
+++ BackEnd/routes/user.js
@@ -2,7 +2,61 @@
 /*
  * GET users listing.
  */
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/geekytodo');
+
+var UserSchema = mongoose.Schema({
+		username 	: String,
+		password 	: String,
+		email			: String,
+		firstName	: String,
+		lastName	: String,
+		mobileNumber : String
+		
+	});
+
+var User = mongoose.model('User', UserSchema);
 
 exports.list = function(req, res){
+	User.find(function(err, users) {
+		console.log(users);
+	});
   res.send("respond with a resource");
 };
+
+exports.signup = function(req, res) {
+	
+	//console.log(req.body);
+
+	var newUser = new User({
+			username : req.body.username,
+			password : req.body.password,
+			email : req.body.email,
+			firstName : req.body.firstName,
+			lastName : req.body.lastName,
+			mobileNumber : req.body.mobileNumber
+		});
+
+	User.find({$or : [ {username : newUser.username}, {email : newUser.email} ]}, function(err, users) {
+		if (users.length == 0) {
+			newUser.save(function(err, savedUser) {
+				if (!err) {
+					res.send({
+						status : "SUCCESS",
+						message : "Register Success."
+					});
+				} else {
+					res.send({
+						status : "FAIL",
+						message : "Register Fail, Database Error!"
+					});
+				}
+			});
+		} else {
+			res.send({
+				status : "FAIL",
+				message : "Register Fail, Username/Email Duplicated!"
+			});
+		}
+	});
+}
