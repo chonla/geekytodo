fac8a6fc9da985662b2a272350f4fcd3295abb54
Kiattisak Anoochitarom <macbaszii@gmail.com>
2013-11-24 15:18:38 +0700

    create and list of category implemented

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -8,6 +8,7 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var category = require('./routes/category');
 
 var app = express();
 
@@ -18,6 +19,7 @@ app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
+app.use(express.bodyParser());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
@@ -32,6 +34,13 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+
+// Category API List
+
+app.get('/api/categories/', category.list)
+app.post('/api/category/', category.create)
+app.put('/api/category', category.update)
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
