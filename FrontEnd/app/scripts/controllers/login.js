c1236a2956bc8efa9613cb02450c34a5f2d62c10
Kanokon Chansom <hippo.ahamayba@gmail.com>
2013-11-30 11:32:21 +0700

    update git ignore, edit file login.js and signin

--- FrontEnd/app/scripts/controllers/login.js
+++ FrontEnd/app/scripts/controllers/login.js
@@ -2,6 +2,7 @@
 
 angular.module('geekyTodoApp')
   .controller('LoginCtrl', function ($scope, $http) {
+    $scope.buttonDisable = true;
     $scope.login = function() {
         var data = {
                     username:$scope.username, 
@@ -10,5 +11,12 @@ angular.module('geekyTodoApp')
        $http.jsonp('http://127.0.0.1:3000/api/users/signin',data).then(function(){
             alert("success");
        });
+    }
+    $scope.checkNull = function(){
+        if ($scope.username && $scope.password) {
+            $scope.buttonDisable = false;
+
+        }
+
     }
   });
