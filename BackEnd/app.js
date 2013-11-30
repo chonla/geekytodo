2a202dd809235d0217c4bfd6f085570e9403a31f
Chonlasith Jucksriporn <chonlasith@gmail.com>
2013-11-30 11:28:06 +0700

    Enable CORS on API

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -37,10 +37,10 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.use(function(req, res, next) {
+app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
